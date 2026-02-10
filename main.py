7a73531ed33c71479b194f9fb1800cf7f29d1271
tam cute pho mai que <tam.nguyenduc@gosmartlog.com>
2026-02-11 01:49:12 +0700

    update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Processor collection')
     processors = dict()
     processors['recognition'] = import_class('processor.recognition_rgb.REC_Processor')
-    processors['recognition_rgb_only'] = import_class('processor.recognition_rgb_only.REC_Processor')
+    processors['recognition_rgb_only'] = import_class('processor.recognition_rgb.REC_Processor')
     processors['recognition_fusion'] = import_class('processor.recognition_fusion.REC_Processor')
     subparsers = parser.add_subparsers(dest='processor')
     for k, p in processors.items():
